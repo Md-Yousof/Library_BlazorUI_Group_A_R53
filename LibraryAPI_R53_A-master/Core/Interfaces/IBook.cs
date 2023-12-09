58855026a265d2b0eb8b6df9138573982bf5d26a
Mohammed Yousof <135439769+Md-Yousof@users.noreply.github.com>
2023-12-09 16:49:14 +0600

    Library Web Api file upload

---
+++ LibraryAPI_R53_A-master/Core/Interfaces/IBook.cs
@@ -0,0 +1,16 @@
+﻿using LibraryAPI_R53_A.Core.Domain;
+using LibraryAPI_R53_A.Core.Repositories;
+
+namespace LibraryAPI_R53_A.Core.Interfaces
+{
+    public interface IBook:IRepository<Book>
+    {
+        public Task<Book?> GetByISBN(string isbn);
+        Task UpdateBookAuthors(Book book, IEnumerable<int> authorIds);
+        Task RemoveAuthorsFromBook(int bookId, IEnumerable<int> authorIdsToRemove);
+        IEnumerable<Book> SearchByPublisher(int pId);
+        IEnumerable<Book> SearchByCategory(int cId);
+        IEnumerable<Book> SearchByAuthor(int aId);
+
+    }
+}
